d0cf68cea6fe3e79b70a71419c3f2b8048bfc3e4
lasen7 <lasen7@naver.com>
2017-01-11 15:58:01 +0900

    Add middleware in index.js  - mongoose  - morgan  - bodyParser

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+import mongoose from 'mongoose';
 
 import api from './routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// setup middleware
+app.use(bodyParser.json());
+
+// setup morgan
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // setup router
 app.use('/api', api);
@@ -15,6 +27,16 @@ app.use((err, req, res, next) => {
     msg: err.message
   });
 });
+
+// Connect mongodb
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+  console.log('Connected to mongod server');
+});
+mongoose.connect(process.env.DB_MONGO);
 
 app.listen(port, () => {
   console.log('Server start port on ', port);
