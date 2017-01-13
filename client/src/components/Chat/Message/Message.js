eaf9aa228f5851b68b8b4aad35316bd2b0914146
lasen7 <lasen7@naver.com>
2017-01-13 09:59:25 +0900

    done: component ui

--- client/src/components/Chat/Message/Message.js
+++ client/src/components/Chat/Message/Message.js
@@ -3,9 +3,32 @@ import React from 'react';
 import './Message.css';
 
 const Message = () => {
+
   return (
-    <div className="wrap">
-      h2
+    <div className="container message">
+      <ul className="message-list">
+        <li>hi</li>
+        <li>hello</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+        <li>good morning</li>
+      </ul>
     </div>
   );
 };
--- client/src/containers/Chat.js
+++ client/src/containers/Chat.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 
-import { Header, Message, Write } from 'components';
 import { Wrapper } from 'components';
 
 class Chat extends Component {
   render() {
     return (
-      <div id="chat" style={{ height: '100%' }}>
-        <Header />
-        <Message />
-        <Write />
-      </div>
+      <Wrapper />
     );
   }
 }
-
-// <Wrapper />
-
-// <Header />
-// <Message />
-// <Write />
 
 export default Chat;
