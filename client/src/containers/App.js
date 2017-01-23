1ea1170fc428caa242e5ebe19ac866d7ad6e8c94
lasen7 <lasen7@naver.com>
2017-01-23 20:04:27 +0900

    Client  - Change socket.io url

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import io from 'socket.io-client';
-const socket = io('http://localhost:3001');
+const socket = io('http://52.79.152.107:3001/');
 
 class App extends Component {
 
